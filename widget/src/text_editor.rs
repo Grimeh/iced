eaf1f1e4ce63cdb05a3a673c5d1f269e63352d6d
Brandon Grimshaw <br@ndon.com.au>
2025-06-21 17:13:03 +1000

    Take `Into<Length>` into `TextEditor::width`

--- widget/src/text_editor.rs
+++ widget/src/text_editor.rs
@@ -174,8 +174,8 @@ where
     }
 
     /// Sets the width of the [`TextEditor`].
-    pub fn width(mut self, width: impl Into<Pixels>) -> Self {
-        self.width = Length::from(width.into());
+    pub fn width(mut self, width: impl Into<Length>) -> Self {
+        self.width = width.into();
         self
     }
 
